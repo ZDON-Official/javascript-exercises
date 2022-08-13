553096102d5b78b87948360bd2fa51bd94712566
Zohaib <zrana1214@gmail.com>
2022-08-13 13:28:38 -0400

    done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(strings) {
+    rev = ''
 
+    var split = strings.split('')
+    split = split.reverse()
+    rev = split.join('')
+
+    return rev
 };
 
 // Do not edit below this line
