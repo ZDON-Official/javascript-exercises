13aac3c5aa220f6136b0b9dd4b43c08baf909e68
Zohaib <zrana1214@gmail.com>
2022-08-13 14:36:08 -0400

    code finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,28 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  // [°C] = ([°F] − 32) × 5⁄9
+  var ans = 0
 
+  
+  ans = temp - 32
+  ans *= 5 / 9
+  
+  return ans.toFixed(1)
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  // [°F] = [°C] × 9⁄5 + 32
+  var ans = 0
 
+
+  ans = temp * 5/9
+  ans += 32
+
+  return ans.toFixed(1)
 };
+
+console.log(ftoc(32));
+console.log(ctof(0));
+console.log(ftoc(100));
 
 // Do not edit below this line
 module.exports = {
