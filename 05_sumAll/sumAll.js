c82c4e541ba74735d86164e8627b838bf3cf3a43
Zohaib <zrana1214@gmail.com>
2022-08-13 13:54:21 -0400

    code done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    var sum = 0
 
+    for(var i=num1;i<=num2;i++){
+        sum += i
+    }
+
+    return sum
 };
-
+console.log(sumAll(1,4))
 // Do not edit below this line
 module.exports = sumAll;
