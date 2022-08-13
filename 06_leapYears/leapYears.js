c93dfa55a4f1845c599e7147ed8280fd569f393a
Zohaib <zrana1214@gmail.com>
2022-08-13 14:28:50 -0400

    code clean up and testing done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,28 +2,15 @@ const leapYears = function(year) {
 
     if(year % 4 === 0){
         // Is a leap year
-        //console.log("true");
         if (year % 100 != 0){
             return true
         }
         if (year % 400 === 0 && (year % 100 === 0)){
-            //console.log("leap");
             return true
         }
     }
-
-
-
     return false
 };
-
-var ans = leapYears(1996)
-console.log(ans);
-console.log(leapYears(1997));
-console.log(leapYears(34992));
-console.log(leapYears(1900));
-console.log(leapYears(1600));
-console.log(leapYears(700));
 
 // Do not edit below this line
 module.exports = leapYears;
