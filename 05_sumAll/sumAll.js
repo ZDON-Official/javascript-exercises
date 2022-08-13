b00a043b39863893af7ee52c88a57923ca96c066
Zohaib <zrana1214@gmail.com>
2022-08-13 14:10:35 -0400

    testing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,19 @@
 const sumAll = function(num1, num2) {
     var sum = 0
-
-    for(var i=num1;i<=num2;i++){
-        sum += i
+    
+    if(num1 < 0 || num2 < 0){ return 'ERROR'}
+    if(Number.isInteger(num1) && Number.isInteger(num2)){ 
+        
+        
+        for(var i=num1;i<=num2;i++){
+            sum += i
+        }
+        
+    } else{
+        return 'ERROR'
     }
-
     return sum
 };
-console.log(sumAll(1,4))
+console.log(sumAll(1,'4000'))
 // Do not edit below this line
 module.exports = sumAll;
