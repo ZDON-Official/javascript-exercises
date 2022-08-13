4467f06b5e9442214bcb2585149b4d0b1d161198
Zohaib <zrana1214@gmail.com>
2022-08-13 14:13:00 -0400

    fully tested

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,7 +3,9 @@ const sumAll = function(num1, num2) {
     
     if(num1 < 0 || num2 < 0){ return 'ERROR'}
     if(Number.isInteger(num1) && Number.isInteger(num2)){ 
-        
+        temp = num1
+        num1 = Math.min(num1, num2)
+        num2 = Math.max(temp, num2)
         
         for(var i=num1;i<=num2;i++){
             sum += i
@@ -14,6 +16,6 @@ const sumAll = function(num1, num2) {
     }
     return sum
 };
-console.log(sumAll(1,'4000'))
+console.log(sumAll(123,1))
 // Do not edit below this line
 module.exports = sumAll;
