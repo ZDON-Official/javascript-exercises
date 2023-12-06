e37868fcaab5fbed30c67f3a7554effcdfcd0b5f
Zohaib <zrana1214@gmail.com>
2023-12-06 14:44:30 -0500

    finished exercise 9 - palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+//   console.log(str);
+  return str === str.split("").reverse().join("");
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// console.log(palindromes("Racecar!"))
