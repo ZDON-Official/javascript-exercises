73cf0cdb5842f0f160802dcc11fe87fca513b27a
Zohaib <zrana1214@gmail.com>
2023-12-06 17:28:41 -0500

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  var list = [1, 1];
 
+  if (Math.sign(n) === -1) {
+    return "OOPS";
+  }
+
+  for (var i = 2; i < n; i++) {
+    list[i] = list[i - 1] + list[i - 2];
+  }
+
+  return list[n - 1];
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+// console.log(fibonacci(6));
