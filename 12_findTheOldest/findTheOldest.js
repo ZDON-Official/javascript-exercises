dc35a25d6459cd2380dfcbc6b3153303c973adbf
Zohaib <zrana1214@gmail.com>
2023-12-06 18:03:49 -0500

    finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,44 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  var oldest = 0;
 
+  return people.reduce((oldest_person, person) => {
+    // console.log(person);
+
+    if ("yearOfDeath" in person) {
+      new_old = person.yearOfDeath - person.yearOfBirth;
+    } else {
+      const curYear = new Date().getFullYear();
+      new_old = curYear - person.yearOfBirth;
+    }
+
+    if (new_old > oldest) {
+      oldest_person = person;
+      oldest = new_old;
+    }
+
+    return oldest_person;
+  }, 0);
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+console.log(findTheOldest(people));
